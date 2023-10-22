3729f083378f00a15f78d226958fcf48d4dc717e
stanlocktop <wlad-test@ukr.net>
2023-10-22 15:25:19 +0300

    task 2

---
+++ Task2/Order.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+// клас ордер
+class Order
+{
+    public int OrderNumber { get; set; }
+    public string CustomerName { get; set; }
+    public double TotalAmount { get; set; }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,159 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Product
-{
-    public int Id { get; }
-    public string Name { get; }
-    public double Price { get; }
-
-    public Product(int id, string name, double price)
-    {
-        Id = id;
-        Name = name;
-        Price = price;
-    }
-}
-
-class Shop
-{
-    private List<Product> availableProducts = new List<Product>();
-
-    public void AddProduct(Product product)
-    {
-        availableProducts.Add(product);
-    }
-
-    public void RemoveProduct(int productId)
-    {
-        availableProducts.RemoveAll(product => product.Id == productId);
-    }
-
-    public Product GetProductById(int productId)
-    {
-        return availableProducts.Find(product => product.Id == productId);
-    }
-
-    public List<Product> GetAllProducts()
-    {
-        return availableProducts;
-    }
-}
-
-class Cart
-{
-    private List<Product> cartItems = new List<Product>();
-
-    public void AddToCart(Product product)
-    {
-        cartItems.Add(product);
-    }
-
-    public void RemoveFromCart(int productId)
-    {
-        cartItems.RemoveAll(product => product.Id == productId);
-    }
-
-    public double GetTotalPrice()
-    {
-        double total = 0;
-        foreach (var item in cartItems)
-        {
-            total += item.Price;
-        }
-        return total;
-    }
-
-    public List<Product> GetCartItems()
-    {
-        return cartItems;
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
     {
-        Shop shop = new Shop();
-        Cart cart = new Cart();
-
-        shop.AddProduct(new Product(1, "Product A", 10.99));
-        shop.AddProduct(new Product(2, "Product B", 5.99));
-        shop.AddProduct(new Product(3, "Product C", 7.49));
+        Queue<Order> orderQueue = new Queue<Order>();
+        int processedOrders = 0;
+        double totalProcessedAmount = 0.0;
 
         while (true)
         {
-            Console.WriteLine("Доступні продукти:");
-            foreach (var product in shop.GetAllProducts())
-            {
-                Console.WriteLine($"{product.Id}. {product.Name} - ${product.Price}");
-            }
-
-            Console.WriteLine("Кошик:");
-            foreach (var cartItem in cart.GetCartItems())
-            {
-                Console.WriteLine($"{cartItem.Name} - ${cartItem.Price}");
-            }
-            Console.WriteLine("Загальна вартість кошика: $" + cart.GetTotalPrice());
-
             Console.WriteLine("Меню:");
-            Console.WriteLine("1. Додати продукт в кошик");
-            Console.WriteLine("2. Видалити продукт з кошика");
-            Console.WriteLine("3. Завершити покупку");
+            Console.WriteLine("1. Додати замовлення");
+            Console.WriteLine("2. Обробити замовлення");
+            Console.WriteLine("3. Вивести інформацію про чергу");
+            Console.WriteLine("4. Вийти");
 
-            Console.Write("Виберіть дію (1/2/3): ");
+            Console.Write("Виберіть опцію: ");
             string choice = Console.ReadLine();
 
             if (choice == "1")
             {
-                Console.Write("Введіть ідентифікатор продукту для додавання до кошика: ");
-                if (int.TryParse(Console.ReadLine(), out int productId))
+                // Додавання замовлення до черги
+                Console.Write("Введіть номер замовлення: ");
+                int orderNumber = int.Parse(Console.ReadLine());
+                Console.Write("Введіть ім'я клієнта: ");
+                string customerName = Console.ReadLine();
+                Console.Write("Введіть загальну суму замовлення: ");
+                double totalAmount = double.Parse(Console.ReadLine());
+
+                Order newOrder = new Order
                 {
-                    Product product = shop.GetProductById(productId);
-                    if (product != null)
-                    {
-                        cart.AddToCart(product);
-                        Console.WriteLine($"{product.Name} додано до кошика.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Продукт з введеним ідентифікатором не знайдено.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Невірний формат ідентифікатора продукту.");
-                }
+                    OrderNumber = orderNumber,
+                    CustomerName = customerName,
+                    TotalAmount = totalAmount
+                };
+
+                orderQueue.Enqueue(newOrder);
+                Console.WriteLine("Замовлення додано до черги.");
             }
             else if (choice == "2")
             {
-                Console.Write("Введіть ідентифікатор продукту для видалення з кошика: ");
-                if (int.TryParse(Console.ReadLine(), out int productId))
+                // Обробка замовлення
+                if (orderQueue.Count > 0)
                 {
-                    Product product = cart.GetCartItems().Find(item => item.Id == productId);
-                    if (product != null)
-                    {
-                        cart.RemoveFromCart(productId);
-                        Console.WriteLine($"{product.Name} видалено з кошика.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Продукт з введеним ідентифікатором не знайдено в кошику.");
-                    }
+                    Order processedOrder = orderQueue.Dequeue();
+                    Console.WriteLine($"Оброблено замовлення {processedOrder.OrderNumber} для {processedOrder.CustomerName} на суму ${processedOrder.TotalAmount}");
+                    processedOrders++;
+                    totalProcessedAmount += processedOrder.TotalAmount;
                 }
                 else
                 {
-                    Console.WriteLine("Невірний формат ідентифікатора продукту.");
+                    Console.WriteLine("Черга порожня. Замовлень для обробки немає.");
                 }
             }
             else if (choice == "3")
             {
-                Console.WriteLine("Завершено оформлення покупки.");
+                // Виведення інформації про чергу
+                Console.WriteLine("Замовлення в черзі:");
+                foreach (var order in orderQueue)
+                {
+                    Console.WriteLine($"Замовлення {order.OrderNumber} для {order.CustomerName} на суму ${order.TotalAmount}");
+                }
+                Console.WriteLine($"Загальна кількість оброблених замовлень: {processedOrders}");
+                Console.WriteLine($"Загальна сума оброблених замовлень: ${totalProcessedAmount}");
+            }
+            else if (choice == "4")
+            {
+                Console.WriteLine("Програма завершила роботу.");
                 break;
             }
             else
             {
-                Console.WriteLine("Невірний вибір. Виберіть один з варіантів меню.");
+                Console.WriteLine("Невірний вибір. Будь ласка, виберіть одну з опцій меню.");
             }
             Console.ReadKey();
         }
